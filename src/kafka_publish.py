4c50c9b47cce256f686ef06b70bd87bd1d653127
Isak Diaz <isakdiaz@gmail.com>
2019-01-19 22:16:51 -0500

    Added String/Twist/Image message types and added utils file

--- src/kafka_publish.py
+++ src/kafka_publish.py
@@ -3,39 +3,52 @@
 import json
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
-from std_msgs.msg import String
+import rospy
+from rospy_message_converter import json_message_converter
+from utils import import_msg_type
 
 class kafka_publish():
 
     def __init__(self):
 
         # initialize node
-        rospy.init("kafka_publish")
+        rospy.init_node("kafka_publish")
         rospy.on_shutdown(self.shutdown)
 
         # Retrieve parameters from launch file
         bootstrap_server = rospy.get_param("~bootstrap_server", "localhost:9092")
-        self.ros_topic = rospy.get_param("~ros_topic", "rosout")
-        self.kafka_topic = rospy.get_param("~kafka_topic", "rosout")
+        self.ros_topic = rospy.get_param("~ros_topic", "test")
+        self.kafka_topic = rospy.get_param("~kafka_topic", "test")
+        self.msg_type = rospy.get_param("~msg_type", "std_msgs/String")
+
 
         # Create kafka producer
         self.producer = KafkaProducer(bootstrap_servers=bootstrap_server, value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
-        # Subscribe to ROS topic of interest
-        rospy.Subscriber(topic, String, self.callback)
+        # ROS does not allow a change in msg type once a topic is created. Therefore the msg
+        # type must be imported and specified ahead of time.
+        msg_func = import_msg_type(self.msg_type)
+
+        # Subscribe to the topic with the chosen imported message type
+        rospy.Subscriber(self.ros_topic, msg_func, self.callback)
+       
+        rospy.logwarn("Using {} MSGs from ROS: {} -> KAFKA: {}".format(self.msg_type, self.ros_topic,self.kafka_topic))
+
 
     def callback(self, msg):
         # Output msg to ROS and send to Kafka server
-        rospy.logdebug("MSG Receved: {}".format(msg)) 
-        self.producer.send(self.kafka_topic, {"data": msg})
+        rospy.logwarn("MSG Receved: {}".format(msg)) 
+        json_str = json_message_converter.convert_ros_message_to_json(msg)
+        self.producer.send(self.kafka_topic, json_str)
         
-    def shutdown(self):
-        rospy.loginfo("Shutting down")
 
     def run(self):
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             rate.sleep()            
+
+    def shutdown(self):
+        rospy.loginfo("Shutting down")
 
 if __name__ == "__main__":
 
--- src/ros_publish.py
+++ src/ros_publish.py
@@ -3,20 +3,23 @@
 import json
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
-from std_msgs.msg import String
+import rospy
+from rospy_message_converter import json_message_converter
+from utils import import_msg_type
 
 class ros_publish():
 
     def __init__(self):
 
         # initialize node
-        rospy.init("ros_publish")
+        rospy.init_node("ros_publish")
         rospy.on_shutdown(self.shutdown)
 
         # Retrieve parameters from launch file
         bootstrap_server = rospy.get_param("~bootstrap_server", "localhost:9092")
-        self.ros_topic = rospy.get_param("~ros_topic", "rosout")
-        self.kafka_topic = rospy.get_param("~kafka_topic", "rosout")
+        self.ros_topic = rospy.get_param("~ros_topic", "test")
+        self.kafka_topic = rospy.get_param("~kafka_topic", "test")
+        self.msg_type = rospy.get_param("~msg_type", "std_msgs/String")
 
         # Create kafka consumer
         self.consumer = KafkaConsumer(self.kafka_topic,
@@ -25,21 +28,32 @@ class ros_publish():
                                     auto_offset_reset='latest',
                                     consumer_timeout_ms=5000)
 
-        # Subscribe to ROS topic of interest
-        self.publisher = rospy.Publisher(ros_topic, String, queue_size=10)
-
+        # Import msg type
+        msg_func = import_msg_type(self.msg_type)
         
-    def shutdown(self):
-        rospy.loginfo("Shutting down")
+        # Subscribe to ROS topic of interest
+        self.publisher = rospy.Publisher(self.ros_topic, msg_func, queue_size=10)
+        rospy.logwarn("Using {} MSGs from KAFKA: {} -> ROS: {}".format(self.msg_type, self.kafka_topic,self.ros_topic))
+        
+
 
     def run(self):
  
         while not rospy.is_shutdown():
-            # When the kafka consumer receives a msg, publish it to the ROS topic
-            for message in self.consumer1:
-                self.publsher.publish(message.value["data"])
+            for msg in self.consumer:
+                # Convert Kafka message to JSON string
+                json_str = json.dumps(msg.value)
+                # Convert JSON to ROS message
+                ros_msg = json_message_converter.convert_json_to_ros_message(self.msg_type, json_str)
+                # Publish to ROS topic
+                self.publisher.publish(ros_msg)
+                rospy.logwarn("Received MSG: {}".format(json_str))
         
         rospy.spin()
+
+
+    def shutdown(self):
+        rospy.loginfo("Shutting down")
 
 if __name__ == "__main__":
 
