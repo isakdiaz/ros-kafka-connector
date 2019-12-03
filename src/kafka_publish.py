51a8cbf1f119e5098a9bdadca9dde383c02b59cb
marbosjo <mbosch@robotnik.es>
2019-12-03 18:00:30 +0100

    dirty version working with schemas

--- src/kafka_publish.py
+++ src/kafka_publish.py
@@ -5,7 +5,12 @@ from kafka import KafkaProducer
 from kafka import KafkaConsumer
 import rospy
 from rospy_message_converter import json_message_converter
-from utils import import_msg_type
+from rospy_message_converter import message_converter
+
+import rosbridge_library.internal.ros_loader as ros_loader
+
+from confluent.schemaregistry.client import CachedSchemaRegistryClient
+from confluent.schemaregistry.serializers import MessageSerializer
 
 class kafka_publish():
 
@@ -18,34 +23,38 @@ class kafka_publish():
         # Retrieve parameters from launch file
         bootstrap_server = rospy.get_param("~bootstrap_server", "localhost:9092")
         self.ros_topic = rospy.get_param("~ros_topic", "test")
-        self.kafka_topic = rospy.get_param("~kafka_topic", "test")
+        self.kafka_topic = rospy.get_param("~kafka_topic", "bar")
         self.msg_type = rospy.get_param("~msg_type", "std_msgs/String")
 
+        self.client = CachedSchemaRegistryClient(url='http://localhost:8081')
+        self.serializer = MessageSerializer(self.client)
 
         # Create kafka producer
-        self.producer = KafkaProducer(bootstrap_servers=bootstrap_server, value_serializer=lambda m: json.dumps(m).encode('ascii'))
+        self.producer = KafkaProducer(bootstrap_servers=bootstrap_server) #, value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
         # ROS does not allow a change in msg type once a topic is created. Therefore the msg
         # type must be imported and specified ahead of time.
-        msg_func = import_msg_type(self.msg_type)
+        msg_func = ros_loader.get_message_class(self.msg_type)
 
         # Subscribe to the topic with the chosen imported message type
         rospy.Subscriber(self.ros_topic, msg_func, self.callback)
-       
         rospy.logwarn("Using {} MSGs from ROS: {} -> KAFKA: {}".format(self.msg_type, self.ros_topic,self.kafka_topic))
 
 
     def callback(self, msg):
         # Output msg to ROS and send to Kafka server
         rospy.logwarn("MSG Receved: {}".format(msg)) 
-        json_str = json_message_converter.convert_ros_message_to_json(msg)
-        self.producer.send(self.kafka_topic, json_str)
-        
+        #json_str = json_message_converter.convert_ros_message_to_json(msg)
+        msg_as_dict = message_converter.convert_ros_message_to_dictionary(msg)
+        # s = self.serializer.encode_record_for_topic(self.kafka_topic, msg_as_dict)
+        self.producer.send(self.kafka_topic, value=s)
+        #self.producer.send(self.kafka_topic, json_str)
+
 
     def run(self):
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            rate.sleep()            
+            rate.sleep()
 
     def shutdown(self):
         rospy.loginfo("Shutting down")
--- src/ros_publish.py
+++ src/ros_publish.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
+import rospy
 import json
+
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
-import rospy
 from rospy_message_converter import json_message_converter
-from utils import import_msg_type
+from rospy_message_converter import message_converter
+
+import rosbridge_library.internal.ros_loader as ros_loader
+
+from confluent.schemaregistry.client import CachedSchemaRegistryClient
+from confluent.schemaregistry.serializers import MessageSerializer
+
+
+from pprint import pprint
 
 class ros_publish():
 
@@ -18,37 +27,38 @@ class ros_publish():
         # Retrieve parameters from launch file
         bootstrap_server = rospy.get_param("~bootstrap_server", "localhost:9092")
         self.ros_topic = rospy.get_param("~ros_topic", "test")
-        self.kafka_topic = rospy.get_param("~kafka_topic", "test")
+        self.kafka_topic = rospy.get_param("~kafka_topic", "bar")
         self.msg_type = rospy.get_param("~msg_type", "std_msgs/String")
+
+        self.client = CachedSchemaRegistryClient(url='http://localhost:8081')
+        self.serializer = MessageSerializer(self.client)
 
         # Create kafka consumer
         self.consumer = KafkaConsumer(self.kafka_topic,
                                     bootstrap_servers=bootstrap_server,
-                                    value_deserializer=lambda m: json.loads(m.decode('ascii')),
-                                    auto_offset_reset='latest',
+                                    #value_deserializer=lambda m: json.loads(m.decode('ascii')),
+                                    auto_offset_reset='earliest',
                                     consumer_timeout_ms=5000)
 
         # Import msg type
-        msg_func = import_msg_type(self.msg_type)
-        
+        msg_func = ros_loader.get_message_class(self.msg_type)
+
         # Subscribe to ROS topic of interest
         self.publisher = rospy.Publisher(self.ros_topic, msg_func, queue_size=10)
         rospy.logwarn("Using {} MSGs from KAFKA: {} -> ROS: {}".format(self.msg_type, self.kafka_topic,self.ros_topic))
-        
-
 
     def run(self):
- 
         while not rospy.is_shutdown():
             for msg in self.consumer:
-                # Convert Kafka message to JSON string
-                json_str = json.dumps(msg.value)
-                # Convert JSON to ROS message
-                ros_msg = json_message_converter.convert_json_to_ros_message(self.msg_type, json_str)
+                #pprint(msg)
+                #pprint(msg.value)
+                pprint(self.serializer.decode_message(msg.value))
+                # Convert Kafka message to Dictionary
+                msg_as_dict = self.serializer.decode_message(msg.value) #json.dumps(msg.value)
+                ros_msg = message_converter.convert_dictionary_to_ros_message(self.msg_type, msg_as_dict)
                 # Publish to ROS topic
                 self.publisher.publish(ros_msg)
-                rospy.logwarn("Received MSG: {}".format(json_str))
-        
+                rospy.logwarn("Received MSG:") #.format(ros_msg))
         rospy.spin()
 
 
